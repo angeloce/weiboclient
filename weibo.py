2861097962709c63db74560f1a756bce37bd7f9a
angeloce <garchek@gmail.com>
2012-07-11 15:32:26 +0800

    s2

--- weibo.py
+++ weibo.py
@@ -12,40 +12,6 @@ import base64
 import hashlib
 
 
-#获取一个保存cookie的对象
-#cj = cookielib.LWPCookieJar()
-##将一个保存cookie对象，和一个HTTP的cookie的处理器绑定
-#cookie_support = urllib2.HTTPCookieProcessor(cj)
-##创建一个opener，将保存了cookie的http处理器，还有设置一个handler用于处理http的URL的打开
-#opener = urllib2.build_opener(cookie_support, urllib2.HTTPHandler)
-##将包含了cookie、http处理器、http的handler的资源和urllib2对象板顶在一起
-#urllib2.install_opener(opener)
-
-
-APP_SECRET = 'e81d78bafc6a27bf973c56ef53c95c20'
-APP_KEY = '1156990956'
-USERNAME = 'weloveboob@sina.cn'
-PASSWORD = 'contrame2012'
-
-postdata = {
-    'entry': 'weibo',
-    'gateway': '1',
-    'from': '',
-    'savestate': '7',
-    'userticket': '1',
-    'ssosimplelogin': '1',
-    'vsnf': '1',
-    'vsnval': '',
-    'su': '',
-    'service': 'miniblog',
-    'servertime': '',
-    'nonce': '',
-    'pwencode': 'wsse',
-    'sp': '',
-    'encoding': 'UTF-8',
-    'url': 'http://weibo.com/ajaxlogin.php?framelogin=1&callback=parent.sinaSSOController.feedBackUrlCallBack',
-    'returntype': 'META'
-}
 
 
 class Cache(object):
@@ -96,10 +62,9 @@ def _request(method, url, data=None, handlers=None, addheaders=None):
     return opener.open(url, data)
 
 class WeiboOauth(object):
-    APP_KEY = '1156990956'
-    APP_SECRET = 'e81d78bafc6a27bf973c56ef53c95c20'
-    REDIRECT_URI = 'http://weloveboob.sinaapp.com/'
-    _cache = Cache()
+    APP_KEY = ''
+    APP_SECRET = ''
+    REDIRECT_URI = ''
 
     def __init__(self, username, password):
         self.username = username
@@ -275,10 +240,5 @@ class WeiboClient(object):
     def post(self, path, params=None):
         return self.request('POST', path, params)
 
-
-client = WeiboClient('weloveboob@sina.cn', 'contrame2012')
-client.login()
-import pprint
-pprint.pprint( client.get('comments/mentions.json', {'uid':client.uid}))
 
 
